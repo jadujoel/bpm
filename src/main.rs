832fdf8a0e12e96820b25ff61731d6a93a561c29
Joel Lof <joel.lof@icloud.com>
2025-06-05 21:46:22 +0200

    batman

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use eframe::egui;
+use std::time::Instant;
+
+struct App {
+    last_press: Instant,
+    bpm: f32,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            last_press: Instant::now(),
+            bpm: 0.0,
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button("Tap").clicked() {
+                let now = Instant::now();
+                let delta = now.duration_since(self.last_press);
+                self.last_press = now;
+
+                if delta.as_secs_f32() < 2.0 {
+                    self.bpm = 60.0 / delta.as_secs_f32();
+                }
+            }
+
+            ui.label(format!("BPM: {:.2}", self.bpm));
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "BPM Button",
+        options,
+        Box::new(|_cc| Ok(Box::<App>::default())),
+    )
+}
